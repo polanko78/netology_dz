7f73aee55ef9174f762808e5e7505a26a6b29c75
Dmitry <polanko78@yandex.ru>
2019-02-19 17:01:55 +0300

    все ок

--- recipes.py
+++ recipes.py
@@ -1,11 +1,45 @@
-# читаем файл
+def main():
+    dish = ''
+    print('Блюда в нашей книге : ', cook_book.keys())
+    dish = input('Введите названия блюд (разделитель пробел) : ')
+    dishes = dish.split(' ')
+    check(dishes)
+    person = input('Введите количество персон: ')
+    get_shop_list_by_dishes(dishes, person)
 
- with open('recipes.txt') as f:
-     while True:
+def check(dishes):
+    for i in dishes:
+        if i not in cook_book.keys():
+            print('Нет таких блюд у нас')
+            main()
+
+
+def get_shop_list_by_dishes(dishes, person):
+    print('Список покупок: ')
+    for i in dishes:
+
+
+
+
+ingr_line =[]
+cook_book ={}
+with open('recipes.txt') as f:
+    while True:
         name = f.readline().strip()
         number = f.readline().strip()
-        while number >= 0:
-            line = f.readline().strip()
-            list_line = line.split(' | ')
-            line_dict ={'ingridient_name': list_line[0], 'quantity': ist_line[1], 'measure': list_line[2]}
-            ingr_line.append(line_dict)
+        if number == '':
+            break
+        if number:
+            number = int(number)
+            while number > 0:
+                line = f.readline().strip()
+                list_line = line.split(' | ')
+                line_dict = {'ingridient_name':list_line[0], 'quantity':list_line[1], 'measure':list_line[2]}
+                ingr_line.append(line_dict)
+                number -= 1
+        cook_book[name] = ingr_line
+        ingr_line = []
+        f.readline()
+
+print(cook_book)
+main()
