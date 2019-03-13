d7da01030a21e08a5516962a6e8af22e4d5c922e
Dmitry <polanko78@yandex.ru>
2019-03-13 10:09:57 +0300

    .

--- vk_bloc1.py
+++ vk_bloc1.py
@@ -20,59 +20,42 @@ class VK_USER:
         return response_gr.json()
 
 
-
-
-
 if __name__ == '__main__':
-
+    group_list = []
+    friend_list = []
+    big_data = []
     token = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
-    user_name = '(eshmargunov)'
     id = 'id (171691064)'
-    name = user_name[1:12]
     user_id = id[4:13]
     user = VK_USER(token, user_id)
     response_fr = requests.get('https://api.vk.com/method/friends.get', user.params)
     response_gr = requests.get('https://api.vk.com/method/groups.get', user.params)
     r_fr = response_fr.json()
     r_gr = response_gr.json()
-#    pprint(r_gr)
-    user_group = set()
     for x in r_gr['response']['items']:
-        user_group.add(x['id'])
-#    user_group = set(r_gr['response']['items']['id'])
+        group_list.append(x['id'])
+    user_group = set(group_list)
     pprint(user_group)
-#    pprint(r_fr['response']['items'])
     for x in r_fr['response']['items']:
+        friend_list.append(x['id'])
+    for x in friend_list:
         user_x = VK_USER(token, x)
-#        response_gr = requests.get('https://api.vk.com/method/groups.get', user_x.params)
         res = user_x.get_groups()
+        tmp_group_list = []
         try:
-            tmp_group = set(res['response']['items'])
+            for x in res['response']['items']:
+                tmp_group_list.append(x['id'])
+            tmp_group = set(tmp_group_list)
             user_group.difference_update(tmp_group)
             print('.', end='', flush=True)
         except KeyError:
             pass
-#        pprint(res)
-    print('\n')
-    pprint(user_group)
-    for x in user_group:
-        params = {
-            'access_token': token,
-            'v': 5.92,
-            'group_id': x
-        }
-        response = requests.get('https://api.vk.com/method/stats.get', params)
-        time.sleep(2)
-        res = response.json()
-        pprint(res)
-
-    # with open('groups.json', 'w') as file:
-    #     for x in user_group:
-    #
-
-
-
-    # user1 = VK_USER(token, r_fr['response']['items'][0])
-    # response_gr = requests.get('https://api.vk.com/method/groups.get', user1.params)
-    # r_gr = response_gr.json()
-    # pprint(r_gr)
+#    print('\n')
+#    pprint(user_group)
+    with open('groups.json', 'w', encoding='UTF-8') as file:
+        for x in r_gr['response']['items']:
+            for y in user_group:
+                if y == x['id']:
+                    data = {'name': x['name'], 'gid': x['id'], 'members_count': x['members_count']}
+                    big_data.append(data)
+        json.dump(big_data, file, ensure_ascii=False, indent=1)
